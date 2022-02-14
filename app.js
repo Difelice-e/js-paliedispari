432fbc1c295dbdd5986d5dabd8c786eab0ff9b13
Difelice-e <difelice.emanuele@gmail.com>
2022-02-14 15:22:17 +0100

    esercizio palindromo

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
+
+// parolaUtente = parolaUtente.replace(/\s/g, "");
+// console.log(parolaUtente);
+
+// const lettere = parolaUtente.split("");
+// lettere.reverse();
+// console.log(lettere);
+
+// const parolaInversa = lettere.join("");
+// console.log(parolaInversa);
+
+// if (parolaUtente === parolaInversa) {
+//     console.log(parolaUtente, parolaInversa, "la parola è un palindromo");
+// } else {
+//     console.log(parolaUtente, parolaInversa, "la parola non è un palindromo");
+// };
+
+let parolaUtente = prompt("inserisci una parola");
+if (eUnPalindromo(parolaUtente) == true) {
+    console.log("è un palindromo");
+} else {
+    console.log("non è un palindromo");
+}
+
+function eUnPalindromo (parolaUtente) {
+    parolaUtente = parolaUtente.replace(/\s/g, "");
+    console.log(parolaUtente);
+
+    const lettere = parolaUtente.split("");
+    lettere.reverse();
+    console.log(lettere);
+
+    const parolaInversa = lettere.join("");
+    console.log(parolaInversa);
+
+    if (parolaUtente === parolaInversa) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+
 // palindromo
 
 // chiedo all'utente una parola
 // inverto l'ordine delle lettere per creare il palindromo
 // controllo che la parola sia palindroma
 // restituisco un messaggio all'utente
+
+
 
 
 
