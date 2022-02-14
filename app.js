8d1d9d1f8e9b244298445845acbf506878f3ef2a
Difelice-e <difelice.emanuele@gmail.com>
2022-02-14 14:48:17 +0100

    struttura

---
+++ app.js
@@ -0,0 +1,17 @@
+// palindromo
+
+// chiedo all'utente una parola
+// inverto l'ordine delle lettere per creare il palindromo
+// controllo che la parola sia palindroma
+// restituisco un messaggio all'utente
+
+
+
+// pari e dispari
+
+// chiedo all'utente di scegliere fra pari e dispari
+// chiedo all'utente un numero da 1 a 5
+// genero un numero random da 1 a 5 per il pc 
+// faccio la somma dei due numeri 
+// stabilisco se la somma è pari o dispari
+// restituisco il messaggio all'utente
